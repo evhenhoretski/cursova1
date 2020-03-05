b386f45dcb1138a3091d3f7933066fe49785f43e
Evhen <gevina4@gmail.com>
2020-03-05 15:27:13 +0200

    dao

--- src/main/java/edu/evhen/cursova/service/photocenter/Impls/PhotocenterServiceImpl.java
+++ src/main/java/edu/evhen/cursova/service/photocenter/Impls/PhotocenterServiceImpl.java
@@ -13,7 +13,7 @@ import java.util.List;
 
 @Service
 public class PhotocenterServiceImpl implements IPhotocenterService {
-    //Hello
+    //Hello word
     @Autowired
     PhotocenterDaoImplFake dao;
 
